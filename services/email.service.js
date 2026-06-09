d2c51c89f93efa68f917f065b5aa3af311638037
Gonzalo Flores <gef.22@hotmail.com>
2026-06-09 00:03:03 -0300

    usar gmail simple

--- services/email.service.js
+++ services/email.service.js
@@ -13,21 +13,16 @@ const enviarEmail = async ({ para, asunto, html }) => {
   console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "OK" : "VACIO");
   console.log("EMAIL_DESTINO:", para);
 
-  try {
-    const info = await transporter.sendMail({
-      from: `"StockAlert" <${process.env.EMAIL_USER}>`,
-      to: para,
-      subject: asunto,
-      html
-    });
+  const info = await transporter.sendMail({
+    from: `"StockAlert" <${process.env.EMAIL_USER}>`,
+    to: para,
+    subject: asunto,
+    html
+  });
 
-    console.log("EMAIL ENVIADO:", info.messageId);
+  console.log("EMAIL ENVIADO:", info.messageId);
 
-    return info;
-  } catch (error) {
-    console.error("ERROR EMAIL:", error.message);
-    throw error;
-  }
+  return info;
 };
 
 module.exports = enviarEmail;
